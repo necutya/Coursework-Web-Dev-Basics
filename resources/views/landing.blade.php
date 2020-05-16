867dd44bd27b09f9c9d1965ff74d0dceaab9ea58
YOZh666 <63615724+YOZh666@users.noreply.github.com>
2020-05-16 22:12:23 +0300

    add My last works

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -1,5 +1,14 @@
 @extends('layouts.layout')
 @section('main')
-    <h1 style="color: black">jopa</h1>
+    <h2 class="works">My last works</h2>
+    <div class="container">
+        <div class="clothes">
+            <img src="images/arr_right.png" class="arrow">
+            <img src="images/first.jpg" class="item">
+            <img src="images/second.jpg" class="item">
+            <img src="images/third.jpg" class="item">
+            <img src="images/arr_left.png" class="arrow">
+        </div>
+    </div>
 @endsection
 
